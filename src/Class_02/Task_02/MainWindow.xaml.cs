26733fd6c94a538284c6f2cbe5e18dede4a6930a
viktor <sterilework@gmail.com>
2025-02-26 00:10:32 +0300

    feat: add class 02 task 02

---
+++ src/Class_02/Task_02/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task_02
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Button myButton = new Button();
+            myButton.Content = "Кнопка";
+            myButton.Width = 120;
+            myButton.Height = 40;
+            myButton.HorizontalAlignment = HorizontalAlignment.Center;
+            myButton.Background = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Colors.Red);
+            layoutPnl.Children.Add(myButton);
+        }
+    }
+}
