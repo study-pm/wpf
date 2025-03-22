d55764b85ec33792e35246dcf8053ab974d06a26
viktor <sterilework@gmail.com>
2025-03-22 23:03:34 +0300

    feat: log text events

--- src/07_Events/RoutedEvents/MainWindow.xaml.cs
+++ src/07_Events/RoutedEvents/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,11 +24,10 @@ namespace RoutedEvents
         public MainWindow()
         {
             InitializeComponent();
-            txtBlk.MouseUp += Element_MouseUp;
-            txtBlk.MouseUp -= Element_MouseUp;
+            textBlk.MouseUp += Element_MouseUp;
+            textBlk.MouseUp -= Element_MouseUp;
         }
 
-        int i = 0;
         private void Element_MouseUp(object sender, MouseButtonEventArgs e)
         {
             MessageBox.Show($"Object: {sender}, Source: {e.Source}, OriginalSource: {e.OriginalSource}, RoutedEvent: {e.RoutedEvent}");
@@ -37,5 +37,100 @@ namespace RoutedEvents
             MessageBox.Show($"Object: {sender}, Source: {e.Source}, OriginalSource: {e.OriginalSource}, RoutedEvent: {e.RoutedEvent}");
         }
 
+        int i = 0;
+
+        private void El_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            i++;
+            string message = "--> " + i + ":\r\n" +
+                "Объект: " + sender.ToString() + "\r\n" +
+                "Событие: " + e.RoutedEvent.ToString() + "\r\n" +
+                "Источник: " + e.Source.ToString() + "\r\n" +
+                "Первоначальный источник: " + e.OriginalSource;
+            lbInfoBubble.Items.Add(message);
+            e.Handled = (bool)chb_ShowFirstEvent.IsChecked;
+        }
+        private void El_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            i++;
+            string message = "--> " + i + ":\r\n" +
+                "Объект: " + sender.ToString() + "\r\n" +
+                "Событие: " + e.RoutedEvent.ToString() + "\r\n" +
+                "Источник: " + e.Source.ToString() + "\r\n" +
+                "Начальный источник: " + e.OriginalSource;
+            lbInfoTunnel.Items.Add(message);
+            // This locks the tunneling sequence thus button stops working
+            // e.Handled = (bool)chb_ShowFirstEvent.IsChecked;
+        }
+
+        private void clearBtn_Click(object sender, RoutedEventArgs e)
+        {
+            lbInfoBubble.Items.Clear();
+            lbInfoTunnel.Items.Clear();
+            i = 0;
+            txtBx.Text = string.Empty;
+        }
+
+        private void DoSomething(object sender, RoutedEventArgs e)
+        {
+            if (e.OriginalSource == opt1)
+            { }
+            else if (e.OriginalSource == opt2)
+            { }
+            else if (e.OriginalSource == opt3)
+            { }
+        }        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            RadioButton selectedRadio = (RadioButton)e.Source;
+            textBlock1.Text = "Вы выбрали: " + selectedRadio.Content.ToString();
+        }
+        private void txtBx_KeyEvt(object sender, KeyEventArgs e)
+        {
+            if ((bool)IgnoreRepeatChkBx.IsChecked && e.IsRepeat) return;
+            i++;
+            string msg = "--> " + i + ":\r\n" +
+                "Событие: " + e.RoutedEvent + "\r\n" +
+                "Клавиша: " + e.Key + "\r\n" +
+                "Системная клавиша: " + e.SystemKey + "\r\n" +
+                "Повтор: " + e.IsRepeat + "\r\n" +
+                "Нажатие: " + e.IsUp + "\r\n" +
+                "Освобождение: " + e.IsDown + "\r\n" +
+                "Включение: " + e.IsToggled + "\r\n" +
+                "Модификаторы: " + e.KeyboardDevice.Modifiers.ToString() + "\r\n" +
+                "Элемент фокуса: " + e.KeyboardDevice.FocusedElement.ToString()
+            ;
+            lbInfoBubble.Items.Add(msg);
+        }
+        private void txtBx_TextInput(object sender, TextCompositionEventArgs e)
+        {
+            // Locked: Never fires, being suppressed by TextBox
+            MessageBox.Show("TextInput");
+        }
+        private void txtBx_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            i++;
+            string msg = "--> " + i + ":\r\n" +
+                "Событие: " + e.RoutedEvent + "\r\n" +
+                "Текст: " + e.Text
+            ;
+            lbInfoBubble.Items.Add(msg);
+        }
+        private void txtBx_GotFocus(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("GotFocus");
+            // txtBx.SelectAll();
+            txtBx.SelectionStart = 0;
+            txtBx.SelectionLength = txtBx.Text.Length;
+        }
+        private void txtBx_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Debug.WriteLine("PreviewMouseDown");
+            if (!txtBx.IsKeyboardFocusWithin)
+            {
+                e.Handled = true;
+                txtBx.Focus();
+                Debug.WriteLine("Focus");
+            }
+        }
     }
 }
